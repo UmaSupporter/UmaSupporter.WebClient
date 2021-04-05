ac88c1a27eafa0894e0d4ef2a2fbc3d541af98df
Basix <basix@basix.tech>
2021-04-05 23:50:40 +0900

    Use document.baseURI instead of hardcoded values

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,18 +1,4 @@
 import { createContext } from "react"
 
-let production = process.env.NODE_ENV === 'production';
-let uri;
-
-if(process.env.URL) {
-  // https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
-  // Required for Netlify Deploy Preview to perform correctly.
-  uri = process.env.URL
-}
-else if(production) {
-  uri = 'https://uma.hitagi.moe'
-}
-else {
-  uri = 'http://localhost:5000'
-}
-export const baseUri = uri
-export const UriContext = createContext(uri)
+export const baseUri = document.baseURI
+export const UriContext = createContext(baseUri)
