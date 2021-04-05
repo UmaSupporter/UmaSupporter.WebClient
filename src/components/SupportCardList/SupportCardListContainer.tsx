45257aace0efedb9b992348d647230f38a898167
Lee Suho <riemannulus@hitagi.moe>
2021-04-06 00:11:30 +0900

    feat: SupportCardListReducer

--- src/components/SupportCardList/SupportCardListContainer.tsx
+++ src/components/SupportCardList/SupportCardListContainer.tsx
@@ -1,10 +1,11 @@
 import gql from "graphql-tag";
-import React from "react";
+import React, { useReducer } from "react";
 import { useSupportCardQuery } from "../../generated/graphql";
 import { CORE_SUPPORT_CARD_FIELD } from "../common/fragments";
-import { SupportCard } from "../../types";
+import { CARD_TYPE, SupportCard } from "../../types";
 import SupportCardList from "./SupportCardList";
 import { rareDegreeCompare } from "../../common/utils";
+import { filterReducer } from "./SupportCardListReducer";
 
 gql`
   ${CORE_SUPPORT_CARD_FIELD}
@@ -23,9 +24,16 @@ type Props = {
 
 const SupportCardListContainer: React.FC<Props> = (props: Props) => {
   const { loading, error, data } = useSupportCardQuery();
+  const [state, dispatch] = useReducer(filterReducer, {filters: new Set<CARD_TYPE>()});
   if (loading) return (<p>loading...</p>)
   if (error) return (<p>error</p>)
   if (data == null || data.supportCard == null) return (<p>data not exist</p>)
+
+
+  const addFilter = (cardType: CARD_TYPE) =>  dispatch({ type: 'SET_FILTER', payload: cardType })
+  const removeFilter = (cardType: CARD_TYPE) =>  dispatch({ type: 'REMOVE_FILTER', payload: cardType })
+  const toggleFilter = (cardType: CARD_TYPE) =>  dispatch({ type: 'TOGGLE_FILTER', payload: cardType })
+  const resetFilter = () =>  dispatch({ type: 'RESET_FILTER' })
 
   let cardList: SupportCard[] = data.supportCard.map(x => {
     return {
---
+++ src/components/SupportCardList/SupportCardListReducer.tsx
@@ -0,0 +1,62 @@
+import { StayCurrentPortraitTwoTone } from "@material-ui/icons"
+import { CARD_TYPE } from "../../types"
+
+type SetFilterAction = {
+  type: 'SET_FILTER',
+  payload: CARD_TYPE
+}
+
+type RemoveFilterAction = {
+  type: 'REMOVE_FILTER',
+  payload: CARD_TYPE
+}
+
+type ToggleFilterAction = {
+  type: "TOGGLE_FILTER",
+  payload: CARD_TYPE
+}
+
+type ResetFilterAction = {
+  type: 'RESET_FILTER',
+}
+
+type Action = SetFilterAction 
+              | RemoveFilterAction 
+              | ToggleFilterAction 
+              | ResetFilterAction 
+
+type State = {
+  filters: Set<CARD_TYPE>,
+}
+
+export const filterReducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'SET_FILTER':
+      return {
+        filters: state.filters.add(action.payload)
+      };
+    case 'REMOVE_FILTER':
+      state.filters.delete(action.payload)
+      return {
+        filters: state.filters
+      };
+    case 'TOGGLE_FILTER':
+      if(state.filters.has(action.payload)) {
+        state.filters.delete(action.payload)
+        return {
+          filters: state.filters
+        };
+      }
+      else {
+        return {
+          filters: state.filters.add(action.payload)
+        };
+      }
+    case 'RESET_FILTER':
+      return {
+        filters: new Set<CARD_TYPE>()
+      };
+    default:
+      throw new Error('Unhandled action');
+  }
+}
