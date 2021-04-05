8e021b7015f4b3582c4fed6e69a9adc1c19c8fa4
Basix <basix@basix.tech>
2021-04-06 00:39:51 +0900

    Replace selectedCardReducer with Zustand store

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect } from "react";
+import create from "zustand";
+
 import SupportCardDetailContainer from "../../components/SupportCardDetail";
 import UmaListContainer from "../../components/UmaList";
 import "./Main.scss";
@@ -8,126 +10,54 @@ import CardListView from "../../views/CardList";
 import logoImage from "../../img/logo.png";
 import donationLogo from "../../img/donation.svg";
 
-type SetUmaAction = {
-  type: 'SET_UMA_ACTION',
-  payload: number
-}
-
-type AppendAction = {
-  type: 'APPEND_CARD',
-  payload: number
-}
-
-type DeleteAction = {
-  type: 'DELETE_CARD',
-  payload: number
-}
-
-type SetCardAction = {
-  type: 'SET_CARD',
-  payload: number
-}
-
-type ToggleUmaPageAction = {
-  type: 'TOGGLE_UMA_PAGE'
-}
-
-type ToggleCardPageAction = {
-  type: 'TOGGLE_CARD_PAGE'
-}
-
-type ResetFavoriteAction = {
-  type: 'RESET_FAVORITE',
-}
-
-const resetState = (): State => {
-  return {
-    favoriteCardUuids: [],
-    cardUuid: 0,
-    umaUuid: 0,
-    showUmaPage: true,
-    showCardPage: true,
-  }
-}
-
-type Action = AppendAction
-  | DeleteAction
-  | ResetFavoriteAction
-  | SetUmaAction
-  | SetCardAction
-  | ToggleUmaPageAction
-  | ToggleCardPageAction
-
-type State = {
+type Store = {
   favoriteCardUuids: number[],
   umaUuid: number,
   cardUuid: number,
   showUmaPage: boolean,
-  showCardPage: boolean
+  showCardPage: boolean,
+  resetFavorite: () => void,
+  toggleFavoriteCard: (uuid: number) => void,
+  deleteFavoriteCard: (uuid: number) => void,
+  setCard: (uuid: number) => void,
+  setUmamusume: (uuid: number) => void,
+  toggleUmaPage: () => any,
+  toggleCardPage: () => any
 }
 
-const selectedCardReducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case 'SET_UMA_ACTION':
-      return {
-        ...state,
-        umaUuid: action.payload,
-        showUmaPage: !state.showUmaPage
-      }
-    case 'APPEND_CARD':
-      if (state.favoriteCardUuids.includes(action.payload))
-        return {
-          ...state,
-          favoriteCardUuids: state.favoriteCardUuids.filter(uuid => uuid !== action.payload),
-        };
-      if (state.favoriteCardUuids.length >= 6) return state;
-      else {
-        return {
-          ...state,
-          favoriteCardUuids: [...state.favoriteCardUuids, action.payload],
-        }
-      }
-    case 'DELETE_CARD':
-      return {
-        ...state,
-        favoriteCardUuids: state.favoriteCardUuids.filter(x => x !== action.payload),
-      };
-    case 'SET_CARD':
-      return {
-        ...state,
-        cardUuid: action.payload,
-        showCardPage: false
-      }
-    case 'RESET_FAVORITE':
-      return {
-        ...state,
-        favoriteCardUuids: []
-      }
-    case 'TOGGLE_UMA_PAGE':
-      return {
-        ...state,
-        showUmaPage: !state.showUmaPage
-      }
-    case 'TOGGLE_CARD_PAGE':
-      return {
-        ...state,
-        showCardPage: !state.showCardPage
-      }
-    default:
-      throw new Error('Unhandled action');
-  }
-}
+const useSelectedCard = create<Store>((set, get) => ({
+  favoriteCardUuids: [],
+  cardUuid: 0,
+  umaUuid: 0,
+  showUmaPage: true,
+  showCardPage: true,
+  resetFavorite: () => set({
+    favoriteCardUuids: [],
+  }, true),
+  toggleFavoriteCard(uuid: number) {
+    Mixpanel.track(TRACK.SET_FAVORITE, {uuid})
+    if(get().favoriteCardUuids.includes(uuid)) {
+      get().deleteFavoriteCard(uuid)
+      return
+    }
+    if(get().favoriteCardUuids.length >= 6) return
+    set(state => ({ favoriteCardUuids: [...state.favoriteCardUuids, uuid] }))
+  },
+  deleteFavoriteCard: (uuid: number) => set(state => ({ favoriteCardUuids: state.favoriteCardUuids.filter(x => x !== uuid) })),
+  setCard(uuid: number) {
+    Mixpanel.track(TRACK.SET_CARD, { uuid })
+    set({ cardUuid: uuid })
+  },
+  setUmamusume(uuid: number) {
+    Mixpanel.track(TRACK.SET_UMA, { uuid })
+    set({ umaUuid: uuid })
+  },
+  toggleUmaPage: () => set(state => ({ showUmaPage: !state.showUmaPage })),
+  toggleCardPage: () => set(state => ({ showCardPage: !state.showCardPage }))
+}))
 
 const Main: React.FC = () => {
-  const [state, dispatch] = useReducer(selectedCardReducer, {}, resetState);
-
-  const toggleFavoriteCard = (uuid: number) => { Mixpanel.track(TRACK.SET_FAVORITE, { uuid: uuid }); dispatch({ type: 'APPEND_CARD', payload: uuid }) }
-  const deleteCard = (uuid: number) => dispatch({ type: 'DELETE_CARD', payload: uuid })
-  const resetCard = () => dispatch({ type: 'RESET_FAVORITE' })
-  const setUmamusume = (uuid: number) => { Mixpanel.track(TRACK.SET_UMA, { uuid: uuid }); dispatch({ type: 'SET_UMA_ACTION', payload: uuid }) }
-  const setCard = (uuid: number) => { Mixpanel.track(TRACK.SET_CARD, { uuid: uuid }); dispatch({ type: 'SET_CARD', payload: uuid }) }
-  const toggleUmaPage = () => dispatch({ type: 'TOGGLE_UMA_PAGE' });
-  const toggleCardPage = () => dispatch({ type: 'TOGGLE_CARD_PAGE' });
+  const state = useSelectedCard()
 
   useEffect(() => {
     Mixpanel.track(TRACK.MAINPAGE, {})
@@ -154,7 +84,7 @@ const Main: React.FC = () => {
       <div className={"MainPageContent"}>
         <div className={`UmaListSection ${state.showUmaPage ? "activated" : ""}`}>
           <UmaListContainer
-            onClickItem={setUmamusume}
+            onClickItem={state.setUmamusume}
             showUmaPage={state.showUmaPage}
           />
         </div>
@@ -163,23 +93,23 @@ const Main: React.FC = () => {
           <div className={`UmaEventChoice EventChoice ${!state.showUmaPage ? "activated" : ""}`}>
             <UmaDetailContainer
               uuid={state.umaUuid}
-              toggleUmaPage={toggleUmaPage}
+              toggleUmaPage={state.toggleUmaPage}
             />
           </div>
 
           <div className={`CardEventChoice EventChoice ${!state.showCardPage ? "activated" : ""}`}>
             <SupportCardDetailContainer
               uuid={state.cardUuid}
-              toggleCardPage={toggleCardPage} />
+              toggleCardPage={state.toggleCardPage} />
           </div>
         </div>
         <div className={`CardListArea ${state.showCardPage ? "activated" : ""}`}>
           <CardListView
             selectedList={state.favoriteCardUuids}
-            onClickItem={setCard}
-            onDeleteItem={deleteCard}
-            onDoubleClickItem={toggleFavoriteCard}
-            onResetItem={resetCard}
+            onClickItem={state.setCard}
+            onDeleteItem={state.deleteFavoriteCard}
+            onDoubleClickItem={state.toggleFavoriteCard}
+            onResetItem={state.resetFavorite}
             showCardPage={state.showCardPage}
           />
         </div>
