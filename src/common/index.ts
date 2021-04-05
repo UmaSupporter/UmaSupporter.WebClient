fdd2676f8cf51afe5e8925c6dfc305a4a5fda38d
Basix <basix@basix.tech>
2021-04-05 22:38:14 +0900

    Fix baseUri on netlify deploy previews

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,7 +2,13 @@ import { createContext } from "react"
 
 let production = process.env.NODE_ENV === 'production';
 let uri;
-if(production) {
+
+if(process.env.URL) {
+  // https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
+  // Required for Netlify Deploy Preview to perform correctly.
+  uri = process.env.URL
+}
+else if(production) {
   uri = 'https://uma.hitagi.moe'
 }
 else {
