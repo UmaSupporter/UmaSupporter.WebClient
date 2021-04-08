1a52ffa4ac0e634199da327355b235e25c9fba11
Lee Suho <riemannulus@hitagi.moe>
2021-04-09 06:08:47 +0900

    feat: parse skill on effect

---
+++ src/components/EventDetail/Effect/Effect.tsx
@@ -0,0 +1,24 @@
+import regexifyString from "regexify-string"
+import Skill from "./Skill"
+
+
+type Props = {
+  effect: string
+}
+
+
+const Effect: React.FC<Props> = (props: Props) => {
+  const { effect } = props;
+
+  const skill = regexifyString({
+    pattern: /(『.*?』)/gi,
+    decorator: (match, index) => {
+        return <Skill key={index} skill={match} />;
+    },
+    input: effect,
+  });
+
+  return<>{skill}</> 
+}
+
+export default Effect
