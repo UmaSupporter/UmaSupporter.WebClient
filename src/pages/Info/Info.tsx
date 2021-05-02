30a71604b4bf0f49b845308b22db1f51ac6c1108
Ben <ben399399@naver.com>
2021-05-03 08:52:46 +0900

    feat: wrap router with scaffold

--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const InfoPage = () => {
-  return <div></div>;
+  return <div>asd</div>
 };
 
 export default InfoPage;
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,11 @@
+import { Scaffold } from './components/common/Scaffold';
 import MainPageRouter from './pages/Main/router';
 
 const MainRouter: React.FC = () => {
   return (
-    <>
+    <Scaffold>
       <MainPageRouter />
-    </>
+    </Scaffold>
   );
 };
 
