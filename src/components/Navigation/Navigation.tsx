12cd85151a24c3c89fb339d8b68834fd693f2fca
Ben <ben399399@naver.com>
2021-05-03 22:52:19 +0900

    feat: new layout with scaffold

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Navigation.scss'
 
 const Navigation = () => {
+  const location = useLocation().pathname;
   return <div className="Navigation">
-    <div className={"highlighter"}></div>
+    <div className={`highlighter ${location}`} />
+    <Link to="/">
+      <div>Event</div>
+    </Link>
+    <Link to="/info">
+      <div>Info</div>
+    </Link>
   </div>;
 };
 
--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Navigation } from '../../components/Navigation';
 
 const InfoPage = () => {
   return <div>asd</div>
