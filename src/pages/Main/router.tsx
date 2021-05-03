02e631223cb59faf3a30b4e8954093a8633895af
Ben <ben399399@naver.com>
2021-05-03 22:52:40 +0900

    fix: rearranged scaffold for working navigation

--- src/pages/Main/router.tsx
+++ src/pages/Main/router.tsx
@@ -1,19 +1,32 @@
 import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { Scaffold } from '../../components/common/Scaffold';
 import { InfoPage } from '../Info';
 import Main from './Main';
 
 const MainPageRouter: React.FC = () => {
-  return <BrowserRouter>
-    <Route
-      exact
-      path="/"
-      component={Main} />
-    <Route
-      exact
-      path="/info"
-      component={InfoPage} />
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Route
+        exact={true}
+        path={"/"}
+        render={() => {
+          return <Scaffold>
+            <Main />
+          </Scaffold>
+        }}
+      />
+      <Route
+        exact={true}
+        path={"/info"}
+        render={() => {
+          return <Scaffold>
+            <InfoPage />
+          </Scaffold>
+        }}
+      />
+    </BrowserRouter>
+  )
 };
 
 export default MainPageRouter;
