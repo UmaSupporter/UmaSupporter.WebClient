9ac2711890e9c330947e02f732c66b98ad7b197e
Ben <ben399399@naver.com>
2021-05-07 01:51:38 +0900

    feat: navigation with mobile layout

---
+++ src/pages/Info/InfoSkill/InfoSkill.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const InfoSkill = () => {
+  return <div className="InfoSkill">infoskill</div>
+}
+export default InfoSkill;
---
+++ src/pages/Info/InfoSkill/index.ts
@@ -0,0 +1,1 @@
+export { default as InfoSkill} from './InfoSkill';
