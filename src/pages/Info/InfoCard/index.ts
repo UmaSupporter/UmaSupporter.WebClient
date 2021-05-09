633da9475008435ccab657305c9931945eb0cb12
Ben Kim <ben399399@naver.com>
2021-05-09 16:03:07 +0900

    refactor: format code

--- src/pages/Info/InfoCard/index.ts
+++ src/pages/Info/InfoCard/index.ts
@@ -1,1 +1,1 @@
-export { default as InfoCardPage } from './InfoCardPage'
+export { default as InfoCardPage } from './InfoCardPage';
--- src/types/index.ts
+++ src/types/index.ts
@@ -54,92 +54,90 @@ export enum CARD_TYPE {
   FRIEND = '友人',
 }
 
-export enum UmaSkillCategoryEnum{
-  ORIGIN='ORIGIN',
-  BASIC='BASIC',
-  AWAKENING='AWAKENING'
+export enum UmaSkillCategoryEnum {
+  ORIGIN = 'ORIGIN',
+  BASIC = 'BASIC',
+  AWAKENING = 'AWAKENING',
 }
 
-export enum SkillGradeEnum{
+export enum SkillGradeEnum {
   NORMAL = 'NORMAL',
   RARE = 'RARE',
-  ORIGINAL = 'ORIGINAL'
+  ORIGINAL = 'ORIGINAL',
 }
 
-export type SkillGradeType={
-  name:SkillGradeEnum
+export type SkillGradeType = {
+  name: SkillGradeEnum;
+};
+
+export enum SkillBuffTypeEnum {
+  ENDURANCE = 'ENDURANCE',
+  ACCELERATION = 'ACCELERATION',
+  VELOCITY = 'VELOCITY',
+  STAMINA = 'STAMINA',
+  INTELLIGENCE = 'INTELLIGENCE',
+  GUTS = 'GUTS',
+  SPEED = 'SPEED',
+  POWER = 'POWER',
+  DEBUF_SPEED = 'DEBUF_SPEED',
+  START = 'START',
+  PROVOCATION = 'PROVOCATION',
+  POSITION = 'POSITION',
+  EYESIGHT = 'EYESIGHT',
+  DEBUF_EYESIGHT = 'DEBUF_EYESIGHT',
+  DEBUF_STAMINA = 'DEBUF_STAMINA',
+  PHYSICAL = 'PHYSICAL',
 }
 
-export enum SkillBuffTypeEnum{
-  ENDURANCE='ENDURANCE',
-  ACCELERATION='ACCELERATION',
-  VELOCITY='VELOCITY',
-  STAMINA='STAMINA',
-  INTELLIGENCE='INTELLIGENCE',
-  GUTS='GUTS',
-  SPEED='SPEED',
-  POWER='POWER',
-  DEBUF_SPEED='DEBUF_SPEED',
-  START='START',
-  PROVOCATION='PROVOCATION',
-  POSITION='POSITION',
-  EYESIGHT='EYESIGHT',
-  DEBUF_EYESIGHT='DEBUF_EYESIGHT',
-  DEBUF_STAMINA='DEBUF_STAMINA',
-  PHYSICAL='PHYSICAL'
-}
+export type SkillBuffType = {
+  name: SkillBuffTypeEnum;
+};
 
-export type SkillBuffType =  {
-  name:SkillBuffTypeEnum
-}
-
-export enum SkillDistanceTypeEnum{
-  SHORT='SHORT',
-  MILE='MILE',
-  MEDIUM='MEDIUM',
-  LONG='LONG',
-  DIRT='DIRT'
+export enum SkillDistanceTypeEnum {
+  SHORT = 'SHORT',
+  MILE = 'MILE',
+  MEDIUM = 'MEDIUM',
+  LONG = 'LONG',
+  DIRT = 'DIRT',
 }
 
 export type SkillDistanceType = {
-  name: SkillDistanceTypeEnum
-}
+  name: SkillDistanceTypeEnum;
+};
 
-export enum SkillOperationTypeEnum{
-  GETAWAY='GETAWAY',
-  PRECEDING='PRECEDING',
-  PRE_ENTRY='PRE_ENTRY',
-  POST_ENTRY='POST_ENTRY'
+export enum SkillOperationTypeEnum {
+  GETAWAY = 'GETAWAY',
+  PRECEDING = 'PRECEDING',
+  PRE_ENTRY = 'PRE_ENTRY',
+  POST_ENTRY = 'POST_ENTRY',
 }
 
 export type SkillOperationType = {
-  name:SkillOperationTypeEnum
-}
+  name: SkillOperationTypeEnum;
+};
 
 export type SkillType = {
-  name:string,
-  nameKr:string,
-  description:string,
-  condition:string,
-  icon:string,
-  grade:SkillGradeType,
-  buffType:SkillBuffType,
-  distanceType?:SkillDistanceType,
-  operationType:SkillOperationType
-}
-
-
+  name: string;
+  nameKr: string;
+  description: string;
+  condition: string;
+  icon: string;
+  grade: SkillGradeType;
+  buffType: SkillBuffType;
+  distanceType?: SkillDistanceType;
+  operationType: SkillOperationType;
+};
 
 export type UmaSkilType = {
-  category:UmaSkillCategoryEnum,
-  skill:SkillType
-}
+  category: UmaSkillCategoryEnum;
+  skill: SkillType;
+};
 
 export type UmaInfo = {
-  umaName:string,
-  umaNameKr:string,
-  secondName:string,
-  secondNameKr:string,
-  umaImage:string,
-  skills:SkillType[]
-}
+  umaName: string;
+  umaNameKr: string;
+  secondName: string;
+  secondNameKr: string;
+  umaImage: string;
+  skills: SkillType[];
+};
