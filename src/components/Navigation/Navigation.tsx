f58cbeee29e5a958939aa625eb436385ff7441a7
Lee, Suho <riemannulus@hitagi.moe>
2021-05-28 15:35:36 +0900

    Revert "feat: new layout with scaffold"
    This reverts commit 95f7bc174774471eb17fbdb8fb341d1d2fb85a72.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
 import './Navigation.scss'
 
 const Navigation = () => {
-  const location = useLocation().pathname;
   return <div className="Navigation">
-    <div className={`highlighter ${location}`} />
-    <Link to="/">
-      <div>Event</div>
-    </Link>
-    <Link to="/info">
-      <div>Info</div>
-    </Link>
+    <div className={"highlighter"}></div>
   </div>;
 };
 
--- src/pages/Info/Info.tsx
+++ src/pages/Info/Info.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Navigation } from '../../components/Navigation';
 
 const InfoPage = () => {
   return <div>asd</div>
